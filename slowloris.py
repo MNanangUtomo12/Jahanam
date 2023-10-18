78e3f977f4500d69cc864a1798c711ae284540cb
MNanangUtomo12 <93698790+MNanangUtomo12@users.noreply.github.com>
2023-10-18 08:07:15 +0700

    Update and rename Slowloris.py to slowloris.py

--- slowloris.py
+++ slowloris.py
@@ -16,7 +16,7 @@ parser.add_argument(
 parser.add_argument(
     "-s",
     "--sockets",
-    default=350,
+    default=9999,
     help="Number of sockets to use in the test",
     type=int,
 )
@@ -56,7 +56,7 @@ parser.add_argument(
 parser.add_argument(
     "--sleeptime",
     dest="sleeptime",
-    default=20,
+    default=5,
     type=int,
     help="Time to sleep between each header sent.",
 )
