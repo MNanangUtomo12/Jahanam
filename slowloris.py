78f07ff4173c9f033598cc39e9b87c2ef34d62de
Masbull <93698790+MNanangUtomo12@users.noreply.github.com>
2023-11-05 06:50:03 +0700

    Update slowloris.py

--- slowloris.py
+++ slowloris.py
@@ -16,7 +16,7 @@ parser.add_argument(
 parser.add_argument(
     "-s",
     "--sockets",
-    default=500,
+    default=150,
     help="Number of sockets to use in the test",
     type=int,
 )
@@ -56,7 +56,7 @@ parser.add_argument(
 parser.add_argument(
     "--sleeptime",
     dest="sleeptime",
-    default=3,
+    default=1,
     type=int,
     help="Time to sleep between each header sent.",
 )
